1c0b7eaf4f156ff40f66c56c2b3b040b3f1dde5b
糞サイト製作所 <123299846+abekenman@users.noreply.github.com>
2023-02-13 08:49:44 +0900

    Add files via upload

---
+++ fuck.js
@@ -0,0 +1,60 @@
+// Add your code here
+var draw;
+function ale() {
+  document.body.onclick = '';
+  //for (i = 0; i <= 1; i--) {if (document.body.requestFullscreen) {document.body.requestFullscre;} else if (document.body.mozRequestFullScreen) {document.body.mozRequestFullScreen();} else if (document.body.webkitRequestFullscreen) {document.body.webkitRequestFullscreen();} else if (document.body.msRequestFullscreen) {document.body.msRequestFullscreen();}}
+  window.setInterval(() => {
+    try {
+      audio.play();
+      if (
+        (document.fullscreenElement !== undefined &&
+          document.fullscreenElement !== null) ||
+        (document.mozFullScreenElement !== undefined &&
+          document.mozFullScreenElement !== null) ||
+        (document.webkitFullscreenElement !== undefined &&
+          document.webkitFullscreenElement !== null) ||
+        (document.webkitCurrentFullScreenElement !== undefined &&
+          document.webkitCurrentFullScreenElement !== null) ||
+        (document.msFullscreenElement !== undefined &&
+          document.msFullscreenElement !== null)
+      ) {
+      } else {
+        alert('逃さん');
+      }
+    } catch (e) {
+      alert(e);
+    }
+  }, 1);
+}
+function del() {
+  setTimeout(ale, 1000);
+}
+window.onload = () => {
+  draw = document.getElementById('a').getContext('2d');
+  draw.font = '350px serif';
+  draw.fillText('逃さん', 0, 500);
+  document.body.onclick = () => {
+    try {
+      const ad = document.getElementById('au').innerHTML;
+      document.getElementById('au').innerHTML = '';
+      document.getElementById('au').innerHTML = ad;
+      const aud = document.getElementById('audio').src;
+      const audio = new Audio(aud);
+      audio.loop = true;
+      del();
+    } catch (e) {
+      alert(e);
+    }
+  };
+  window.document.onclick = () => {
+    if (document.body.requestFullscreen) {
+      document.body.requestFullscreen();
+    } else if (document.body.mozRequestFullScreen) {
+      document.body.mozRequestFullScreen();
+    } else if (document.body.webkitRequestFullscreen) {
+      document.body.webkitRequestFullscreen();
+    } else if (document.body.msRequestFullscreen) {
+      document.body.msRequestFullscreen();
+    }
+  };
+};

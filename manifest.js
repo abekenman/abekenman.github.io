2c7c090d6b5a492a7823dcd8851d7d22cd18f748
糞サイト製作所 <123299846+abekenman@users.noreply.github.com>
2023-02-15 17:46:37 +0900

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'pwa-sample-caches-v1';
+// Cache targets
+const urlsToCache = [
+  './',
+  './index.html',
+  './script.js',
+  './scr.json',
+  './he.html',
+  './fuck.js',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return response ? response : fetch(event.request);
+      })
+  );
+});

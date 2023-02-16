526c08bb6a8134770875ca0a5327e8fa1fd4e67f
糞サイト製作所 <123299846+abekenman@users.noreply.github.com>
2023-02-16 23:20:34 +0900

    Update fuck.js

--- fuck.js
+++ fuck.js
@@ -5,7 +5,6 @@ function ale() {
   //for (i = 0; i <= 1; i--) {if (document.body.requestFullscreen) {document.body.requestFullscre;} else if (document.body.mozRequestFullScreen) {document.body.mozRequestFullScreen();} else if (document.body.webkitRequestFullscreen) {document.body.webkitRequestFullscreen();} else if (document.body.msRequestFullscreen) {document.body.msRequestFullscreen();}}
   window.setInterval(() => {
     try {
-      alert('逃さん');
       audio.play();
       if (
         (document.fullscreenElement !== undefined &&

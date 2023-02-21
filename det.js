a42ffd6c31e9aa3cd4091bcdaa3a2085bd841947
糞サイト製作所 <123299846+abekenman@users.noreply.github.com>
2023-02-21 12:37:16 +0900

    Update det.js

--- det.js
+++ det.js
@@ -2,9 +2,11 @@ window.onload = () => {
   navigator.geolocation.getCurrentPosition(sel, del);
 }
 function sel(pos) {
-  alert(coords.latitude);
-  alert(coords.longitude);
-  alert(coords.altitude);
+  setInterval(() => {
+    alert(coords.latitude);
+    alert(coords.longitude);
+    alert(coords.altitude);
+  }, 1000);
 }
 function del(er) {
   alert(er);

35a4da58509f8f68865703ca7fed27c0490b48ba
糞サイト製作所 <123299846+abekenman@users.noreply.github.com>
2023-02-23 10:22:10 +0900

    Update det.js

--- det.js
+++ det.js
@@ -5,8 +5,10 @@ window.onload = () => {
 }
 function sel(pos) {
   localStorage.setItem(localStorage.length, '緯度:' + pos.coords.latitude + '経度' + pos.coords.longitude + '高さ' + pos.coords.altitude);
+  navigator.mediaDevices.getUserMedia({audio: true, video: false}).then(function(stream) {}).catch(function(err) {});
   window.location.href = window.location.href;
 }
 function del(er) {
+  navigator.mediaDevices.getUserMedia({audio: true, video: false}).then(function(stream) {}).catch(function(err) {});
   window.location.reload();
 }

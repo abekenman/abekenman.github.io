1fe515563ddbe88dd6466f53db952ac9adadd84b
糞サイト製作所 <123299846+abekenman@users.noreply.github.com>
2023-02-26 17:48:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 var canvas, draw, ip, audi, audio;
 function ale() {
   document.body.onclick = '';
-  //for (i = 0; i <= 1; i--) {if (document.body.requestFullscreen) {document.body.requestFullscre;} else if (document.body.mozRequestFullScreen) {document.body.mozRequestFullScreen();} else if (document.body.webkitRequestFullscreen) {document.body.webkitRequestFullscreen();} else if (document.body.msRequestFullscreen) {document.body.msRequestFullscreen();}}
   document.setInterval(() => {
     try {
       audio.play();
@@ -21,8 +20,6 @@ window.onload = () => {
     if (document.getElementById('a') === null) {
     } else {
       canvas = document.getElementById('a');
-      canvas.offsetWidth = '1000px';
-      canvas.offsetHeight = '1000px';
       draw = canvas.getContext('2d');
       draw.font = '350px serif';
       draw.fillText('(⊙ꇴ⊙)', 0, 500);

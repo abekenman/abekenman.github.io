557976988bd5adee77a337dd4175d1672c2d29fb
糞サイト製作所 <123299846+abekenman@users.noreply.github.com>
2023-02-26 16:38:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var canvas, draw, ip, audi;
 function ale() {
   document.body.onclick = '';
   //for (i = 0; i <= 1; i--) {if (document.body.requestFullscreen) {document.body.requestFullscre;} else if (document.body.mozRequestFullScreen) {document.body.mozRequestFullScreen();} else if (document.body.webkitRequestFullscreen) {document.body.webkitRequestFullscreen();} else if (document.body.msRequestFullscreen) {document.body.msRequestFullscreen();}}
-  window.setInterval(() => {
+  document.setInterval(() => {
     try {
       audio.play();
       if (

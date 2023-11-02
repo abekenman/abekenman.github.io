dc611d3face4a86cef43eb9ec1d13da4317c99bb
Abekenman <123299846+abekenman@users.noreply.github.com>
2023-11-02 11:44:05 +0900

    Update ww.js

--- editor/ww.js
+++ editor/ww.js
@@ -1,6 +1,6 @@
 var start = { interval, value, first};
 onmessage = m => {
-  if(m.data.title == 'start') start.interval = setInterval(() => 
+  if(m.data.title == 'start') start.interval = setInterval(() => {
     if(start.first != e.data.value.data || start.value.first + 1 != e.data.value.data) postMessage({ title: 'start', value: start.value = e.data.value.data + 1 })
-    else postMessage({ title: 'start', value: start.value++ }), m.data.value.fps)
+    else postMessage({ title: 'start', value: start.value++ })}, m.data.value.fps
 }

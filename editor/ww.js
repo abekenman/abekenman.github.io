3e6c30322ad91b8a88d02ed2b6779c75125bc122
Abekenman <123299846+abekenman@users.noreply.github.com>
2023-11-05 13:00:38 +0900

    Update ww.js

--- editor/ww.js
+++ editor/ww.js
@@ -1,5 +1,12 @@
-var start = { interval, value, first};
+var start = { interval: 0, number: 0, long: 0, time: 0};
 onmessage = m => {
   if(m.data.title == 'start') {
+    start.interval = m.data.value.interval;
+    start.number = m.data.value.number;
+    start.long = m.data.value.long;
+    start.time = 0;
+    setInterval(() => {
+      start.time++;
+    }, start.interval);
   }
 }

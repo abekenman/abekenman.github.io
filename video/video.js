b6f9a26cf3eadaaca3fa1a9a1cb940c4057ca130
Abekenman <123299846+abekenman@users.noreply.github.com>
2023-11-05 19:24:42 +0900

    Update video.js

--- video/video.js
+++ video/video.js
@@ -2,12 +2,12 @@
 
 var canvas, draw, video, vin;
 onload = () => {
-  canvas = document.querySelector('canvas');
-  vin = document.querySelector('video');
-  draw = canvas.getContext('2d');
-  draw.fillRect(0, 0, 1000, 1000);
-  const cap = document.querySelector("canvas").captureStream();
   try {
+    canvas = document.querySelector('canvas');
+    vin = document.querySelector('video');
+    draw = canvas.getContext('2d');
+    draw.fillRect(0, 0, 1000, 1000);
+    const cap = document.querySelector("canvas").captureStream();
     video = new MediaRecorder(cap, { mimeType: 'video/mp4'});
     vin.srcObject = cap;
   } catch (e) {

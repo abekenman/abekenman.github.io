7fa7584f0ac0a22a764d597e8364e960cbac3dca
Abekenman <123299846+abekenman@users.noreply.github.com>
2024-01-06 15:45:18 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,16 +10,17 @@ const caches = {
 };
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches
-      .open(caches.version)
-      .then((cache) => return cache.addAll(caches.files))
+    caches.open(caches.version).then((cache) => {
+      return cache.addAll(caches.files)
+    })
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches
-      .match(event.request)
-      .then((response) => return response ? response : fetch(event.request))
+    caches.match(event.request).then((response) => {
+        return response ? response : fetch(event.request)
+
+    })
   );
 });
